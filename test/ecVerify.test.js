124883168a1954f499d812a3e4065601239bbfc1
Dennison Bertram <dennison@dennisonbertram.com>
2019-02-24 17:10:13 -0500

    Truffle tests for the code

--- test/ecVerify.test.js
+++ test/ecVerify.test.js
@@ -2,12 +2,7 @@ const ecVerify = artifacts.require("ecVerify");
 const EthCrypto = require("eth-crypto");
 
 contract("ecVerify", accounts => {
-  xit("Should pass this test", async () => {
-    console.log(ecVerify);
-    assert.equal(1, 1, "simple test passed");
-  });
-
-  it("Should return the signing address", async () => {
+  it("Should return the correct signing address", async () => {
     const signerIdentity = EthCrypto.createIdentity();
     const message = EthCrypto.hash.keccak256([
       { type: "uint256", value: "5" },
@@ -15,8 +10,43 @@ contract("ecVerify", accounts => {
     ]);
     const signature = EthCrypto.sign(signerIdentity.privateKey, message);
 
-    const exVerifyInstance = await ecVerify.deployed();
-    const address = await exVerifyInstance.recoverSigner(message, signature);
+    const ecVerifyInstance = await ecVerify.deployed();
+    const address = await ecVerifyInstance.recoverSigner(message, signature);
     assert.equal(address, signerIdentity.address, "Address returned");
+  });
+
+  it("Should return true when the correct address is passed.", async () => {
+    const signerIdentity = EthCrypto.createIdentity();
+    const message = EthCrypto.hash.keccak256([
+      { type: "uint256", value: "5" },
+      { type: "string", value: "Banana" }
+    ]);
+    const signature = EthCrypto.sign(signerIdentity.privateKey, message);
+
+    const ecVerifyInstance = await ecVerify.deployed();
+    const returnValue = await ecVerifyInstance.validateSignature(
+      message,
+      signature,
+      signerIdentity.address
+    );
+    assert.equal(true, returnValue);
+  });
+
+  it("Should return false when the incorrect address is passed.", async () => {
+    const signerIdentity = EthCrypto.createIdentity();
+    const secondIdentity = EthCrypto.createIdentity();
+    const message = EthCrypto.hash.keccak256([
+      { type: "uint256", value: "5" },
+      { type: "string", value: "Banana" }
+    ]);
+    const signature = EthCrypto.sign(signerIdentity.privateKey, message);
+
+    const ecVerifyInstance = await ecVerify.deployed();
+    const returnValue = await ecVerifyInstance.validateSignature(
+      message,
+      signature,
+      secondIdentity.address
+    );
+    assert.equal(false, returnValue);
   });
 });
